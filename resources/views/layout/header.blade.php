be08aa2adaff690893865ac4462a966c8cef50c6
RaymountAb <raymount_abdahu@student.untan.ac.id>
2024-03-03 16:12:42 +0700

    update css

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,85 @@
+<header>
+    <div class="top">
+        <div class="container">
+            <div class="row">
+                <div class="span6">
+                    <p class="topcontact"><i class="icon-phone"></i> +62 088 999 123</p>
+                </div>
+                <div class="span6">
+
+                    <ul class="social-network">
+                        <li><a href="#" data-placement="bottom" title="Facebook"><i
+                                    class="icon-facebook icon-white"></i></a></li>
+                        <li><a href="#" data-placement="bottom" title="Twitter"><i
+                                    class="icon-twitter icon-white"></i></a></li>
+                        <li><a href="#" data-placement="bottom" title="Linkedin"><i
+                                    class="icon-linkedin icon-white"></i></a></li>
+                        <li><a href="#" data-placement="bottom" title="Pinterest"><i
+                                    class="icon-pinterest  icon-white"></i></a></li>
+                        <li><a href="#" data-placement="bottom" title="Google +"><i
+                                    class="icon-google-plus icon-white"></i></a></li>
+                        <li><a href="#" data-placement="bottom" title="Dribbble"><i
+                                    class="icon-dribbble icon-white"></i></a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+
+
+        <div class="row nomargin">
+            <div class="span4">
+                <div class="logo">
+                    <a href="index.html"><img src="assets/img/logo.png" alt="" /></a>
+                </div>
+                <div class="nama-desa">
+                    Desa Istana <br> Kabupaten Ketapang
+                </div>
+            </div>
+            <div class="span8">
+                <div class="navbar navbar-static-top">
+                    <div class="navigation">
+                        <nav>
+                            <ul class="nav topnav">
+                                <li>
+                                    <a href="contact.html">Beranda </a>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#">Pemerintah Desa<i class="icon-angle-down"></i></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="typography.html">Typography</a></li>
+                                        <li><a href="components.html">Components</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#">Profil Desa <i class="icon-angle-down"></i></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="about.html">About us</a></li>
+                                        <li><a href="faq.html">FAQ</a></li>
+                                        <li><a href="team.html">Team</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#">Regulasi<i class="icon-angle-down"></i></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="portfolio-2cols.html">Portfolio 2 columns</a></li>
+                                        <li><a href="portfolio-3cols.html">Portfolio 3 columns</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="contact.html">Peta </a>
+                                </li>
+                                <li>
+                                    <a href="contact.html">Lapak Desa </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <!-- end navigation -->
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
