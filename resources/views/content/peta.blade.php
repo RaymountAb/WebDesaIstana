7e3f3c870f697ce008df0a68eba09355c1a1d42c
RaymountAb <raymount_abdahu@student.untan.ac.id>
2024-03-07 12:01:05 +0700

    update maps

--- resources/views/content/peta.blade.php
+++ resources/views/content/peta.blade.php
@@ -14,6 +14,6 @@
     </section>
 
     <section id="content">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.89530440337!2d110.52879157397386!3d-1.2324634355675839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e04710fd157f90b%3A0x72c31b511e7e49a1!2sDesa%20istana!5e0!3m2!1sid!2sid!4v1709562131355!5m2!1sid!2sid" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8996560729224!2d110.53025180734261!3d-1.2295545928851037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e0470bc1ffd589b%3A0xdac9ba8ecbeaea42!2sKantor%20Desa%20Istana!5e0!3m2!1sid!2sid!4v1709787389819!5m2!1sid!2sid" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
       </section>
 @endsection
