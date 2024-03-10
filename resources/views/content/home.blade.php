6f7244b89e2e7efaff61300249eaef325c7b3c59
RaymountAb <raymount_abdahu@student.untan.ac.id>
2024-03-10 12:46:14 +0700

    update post

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -25,21 +25,27 @@
                             <div class="span8">
                                 <div class="post-image">
                                     <div class="post-heading">
-                                        <h3><a href="#">Judul Berita Desa</a></h3>
+                                        <h3><a href="#">PELATIHAN UP2K TP. PKK Desa Istana</a></h3>
                                     </div>
 
-                                    <img src="assets/img/post/post1.jpg" alt="" />
+                                    <img src="assets/img/post/post1.png" alt="" />
                                 </div>
                                 <div class="meta-post">
                                     <ul>
                                         <li><i class="icon-file"></i></li>
                                         <li>By <a href="#" class="author">Admin</a></li>
-                                        <li>On <a href="#" class="date">10 Jun, 2013</a></li>
+                                        <li>On <a href="#" class="date">10 Maret, 2024</a></li>
                                     </ul>
                                 </div>
                                 <div class="post-entry">
                                     <p>
-                                        ini merupakan penjelasan berita tentang desa istana yang terbaru
+                                        Kegiatan Pelatihan ini merupakan salah satu program kerja pokja II Kabupaten
+                                        Ketapang yang bertujuan untuk meningkatkan SDM, agar kader PKK dapat terus
+                                        berinovasi dalam memberikan edukasi baru kepada kelompok usaha baru dalam
+                                        pengembangan ekonomi kreatif bagi para kader UP2K. Sementara itu, anggota PKK Desa
+                                        Istana yang ikut mewakili Kecamatan Sandai dalam kegiatan tersebut berkesempatan
+                                        untuk mengenalkan produk unggulan yang dimiliki yaitu Kopi Kampung dan diberikan
+                                        binaan secara langsung oleh PKK Kabupaten Ketapang
                                     </p>
                                 </div>
                             </div>
@@ -52,21 +58,26 @@
                             <div class="span8">
                                 <div class="post-image">
                                     <div class="post-heading">
-                                        <h3><a href="#">Judul Berita Desa</a></h3>
+                                        <h3><a href="#">PENCEGAHAN STUNTING BAGI CALON PENGANTIN</a></h3>
                                     </div>
 
-                                    <img src="assets/img/post/post2.jpg" alt="" />
+                                    <img src="assets/img/post/post2.png" alt="" />
                                 </div>
                                 <div class="meta-post">
                                     <ul>
                                         <li><i class="icon-file"></i></li>
                                         <li>By <a href="#" class="author">Admin</a></li>
-                                        <li>On <a href="#" class="date">10 Jun, 2013</a></li>
+                                        <li>On <a href="#" class="date">10 Maret, 2024</a></li>
                                     </ul>
                                 </div>
                                 <div class="post-entry">
                                     <p>
-                                        ini merupakan penjelasan berita tentang desa istana yang terbaru
+                                        Kegiatan ini dilakukan oleh pihak petugas kesehatan puskesmas kecamatan sandai yang
+                                        dihadiri Kader PKK, Orangtua CATIN, dan Calon Pengantin. Kegiatan ini bertujuan
+                                        memberikan edukasi kepada calon pengantin mengenai bagaimana cara untuk pencegah
+                                        stunting pada balita. Setelah mengikuti kegiatan ini peserta kegiatan menjadi lebih
+                                        memahami cara mencegah terjadinya stunting dan paham tentang langkah yang tepat
+                                        menurunkan persentase tingkat stunting pada bayi dan balita
                                     </p>
                                 </div>
                             </div>
@@ -79,21 +90,122 @@
                             <div class="span8">
                                 <div class="post-image">
                                     <div class="post-heading">
-                                        <h3><a href="#">Judul Berita Desa</a></h3>
+                                        <h3><a href="#">KEGIATAN PEMBENTUKAN KELOMPOK KADARKUM</a></h3>
                                     </div>
 
-                                    <img src="assets/img/post/post3.jpg" alt="" />
+                                    <img src="assets/img/post/post3.png" alt="" />
                                 </div>
                                 <div class="meta-post">
                                     <ul>
                                         <li><i class="icon-file"></i></li>
                                         <li>By <a href="#" class="author">Admin</a></li>
-                                        <li>On <a href="#" class="date">10 Jun, 2013</a></li>
+                                        <li>On <a href="#" class="date">10 Maret, 2024</a></li>
                                     </ul>
                                 </div>
                                 <div class="post-entry">
                                     <p>
-                                        ini merupakan penjelasan berita tentang desa istana yang terbaru
+                                        TP. PKK Desa Istana melaksanakan Pembentukan Kelompok KADARKUM dengan didampingi TP.
+                                        PKK Kecamatan Sandai di Aula Gedung Kesenian Desa Istana. Tujuan kegiatan ini untuk
+                                        meningkatkan kesadaran hokum masyarakat dalam rangka masyarakat berbudaya hokum.
+                                        Selanjutnya dengan terbentuknya kelompok KADARKUM ini, diharapkan setia anggota
+                                        mengetahui dan sadar akan hak dan kewajibannyasebagai Warga Negara Indonesia serta
+                                        mentaati aturan hukum yang berlaku.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article>
+                        <div class="row">
+
+                            <div class="span8">
+                                <div class="post-image">
+                                    <div class="post-heading">
+                                        <h3><a href="#">PERTEMUAN RUTIN DAN PERSIAPAN LOMBA DESA</a></h3>
+                                    </div>
+
+                                    <img src="assets/img/post/post4.png" alt="" />
+                                </div>
+                                <div class="meta-post">
+                                    <ul>
+                                        <li><i class="icon-file"></i></li>
+                                        <li>By <a href="#" class="author">Admin</a></li>
+                                        <li>On <a href="#" class="date">10 Maret, 2024</a></li>
+                                    </ul>
+                                </div>
+                                <div class="post-entry">
+                                    <p>
+                                        Dalam rangka persiapan Lomba Desa yang diselenggarakan oleh Dinas Pemberdayaan
+                                        Masyarakat dan Desa Kabupaten Ketapang, pihak TP. PKK Desa Istana mengadakan
+                                        pertemuan rutin untuk membahas persiapan lomba desa dengan didampingi pihak TP. PKK
+                                        Kecamatan beserta anggotanya untuk melakukan pembinaan agar dapat memaksimalkan
+                                        peran PKK Desa pada saat kegiatan Lomba Desa berlangsung.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article>
+                        <div class="row">
+
+                            <div class="span8">
+                                <div class="post-image">
+                                    <div class="post-heading">
+                                        <h3><a href="#">KEGIATAN GOTONG ROYONG DI KEBUN PKK</a></h3>
+                                    </div>
+
+                                    <img src="assets/img/post/post5.png" alt="" />
+                                </div>
+                                <div class="meta-post">
+                                    <ul>
+                                        <li><i class="icon-file"></i></li>
+                                        <li>By <a href="#" class="author">Admin</a></li>
+                                        <li>On <a href="#" class="date">10 Maret, 2024</a></li>
+                                    </ul>
+                                </div>
+                                <div class="post-entry">
+                                    <p>
+                                        Salah satu bentuk pengamalan 10 program pokok PKK yaitu gotong royong, itulah yang
+                                        saat ini dilakukan ibu-ibu anggota TP. PKK Desa Istana sebagai bentuk pengamalan
+                                        10 program pokok PKK tersebut. Gotong royong ini guna untuk mewujudkan program
+                                        kelestarian lingkungan hidup yaitu membuat kebun untuk bercocok tanam. Kegiatan ini
+                                        dihadiri Ibu Ketua TP. PKK Desa Istana, dan seluruh ketua pokja 1 sampai dengan
+                                        pokja 4 beserta anggotanya.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article>
+                        <div class="row">
+
+                            <div class="span8">
+                                <div class="post-image">
+                                    <div class="post-heading">
+                                        <h3><a href="#">PENYULUHAN PENTINGNYA ASI EKSKLUSIF</a></h3>
+                                    </div>
+
+                                    <img src="assets/img/post/post5.png" alt="" />
+                                </div>
+                                <div class="meta-post">
+                                    <ul>
+                                        <li><i class="icon-file"></i></li>
+                                        <li>By <a href="#" class="author">Admin</a></li>
+                                        <li>On <a href="#" class="date">10 Maret, 2024</a></li>
+                                    </ul>
+                                </div>
+                                <div class="post-entry">
+                                    <p>
+                                        Posyandu Bayi dan Balita di Desa Istana rutin melaksanakan kegiatan posyandu setiap
+                                        bulannya. Mulai dari pengecekan gizi hingga penyuluhan terhadap orangtua bayi dan
+                                        balita yang ada di Desa Istana. Kali ini, pihak puskesmas setempat memberikan
+                                        penyuluhan mengenai pentingnya pemberian ASI Eksklusif. Hal ini dilakukan karena
+                                        terdapat beberapa warga yang tidak memberikan ASI Eksklusif kepada anaknya dengan
+                                        alasan sibuk bekerja, ASI tidak dapat keluar dan lain-lain. Warga sangat antusias
+                                        mengikuti kegiatan ini dan berharap bisa berdiskusi lebih banyak lagi.
                                     </p>
                                 </div>
                             </div>
@@ -108,12 +220,14 @@
                                         <h3><a href="#">Video Profil Desa Istana</a></h3>
                                     </div>
                                     <div class="video-container">
-                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/9r0YjRlZv2A" frameborder="0" allowfullscreen></iframe>
+                                        <iframe width="560" height="315"
+                                            src="https://www.youtube.com/embed/9r0YjRlZv2A" frameborder="0"
+                                            allowfullscreen></iframe>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </article>                    
+                    </article>
                 </div>
 
                 <div class="span4">
@@ -149,25 +263,30 @@
                                 <li><i class="icon-angle-right"></i> <a href="#">Tentang Desa</a><span> (12)</span>
                                 </li>
                             </ul>
-                        </div>               
+                        </div>
 
                         <div class="widget">
                             <h5 class="widgetheading"><i class="fas fa-chart-pie"></i> Statistik Penduduk Desa</h5>
                             <canvas id="populationChart" width="50" height="50"></canvas>
                             <div class="chart-legend">
-                                <span class="legend-item"><span class="legend-color" style="background-color: rgba(255, 99, 132, 0.5);"></span>Perempuan</span>
-                                <span class="legend-item"><span class="legend-color" style="background-color: rgba(54, 162, 235, 0.5);"></span>Laki-laki</span>
+                                <span class="legend-item"><span class="legend-color"
+                                        style="background-color: rgba(255, 99, 132, 0.5);"></span>Perempuan</span>
+                                <span class="legend-item"><span class="legend-color"
+                                        style="background-color: rgba(54, 162, 235, 0.5);"></span>Laki-laki</span>
                             </div>
                         </div>
-                                            
+
 
                         <div class="widget">
-                            <h5 class="widgetheading" style="text-align: left;"><i class="fas fa-user-circle"></i>Masuk</h5>
+                            <h5 class="widgetheading" style="text-align: left;"><i class="fas fa-user-circle"></i>Masuk
+                            </h5>
                             <div style="text-align: center;">
-                                <a href="/login-admin" class="btn btn-primary" style="margin-bottom: 5px;"><i class="fas fa-id-badge "></i> Admin</a><br>
-                                <a href="your link" class="btn btn-success btn-rounded" style="margin-top: 5px;"><i class="fas fa-handshake"></i>Layanan Mandiri</a>
+                                <a href="/login-admin" class="btn btn-primary" style="margin-bottom: 5px;"><i
+                                        class="fas fa-id-badge "></i> Admin</a><br>
+                                <a href="your link" class="btn btn-success btn-rounded" style="margin-top: 5px;"><i
+                                        class="fas fa-handshake"></i>Layanan Mandiri</a>
                             </div>
-                        </div>                                                                                       
+                        </div>
                     </aside>
                 </div>
 
@@ -175,38 +294,38 @@
         </div>
     </section>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script>
-    var ctx = document.getElementById('populationChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: ['Laki-laki', 'Perempuan'],
-            datasets: [{
-                data: [178, 218], // Ganti dengan data jumlah penduduk laki-laki dan perempuan
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.5)', // Warna untuk laki-laki 
-                    'rgba(54, 162, 235, 0.5)',  // Warna untuk perempuan
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            legend: {
-                display: false // Sembunyikan legenda bawaan
+    <script>
+        var ctx = document.getElementById('populationChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Laki-laki', 'Perempuan'],
+                datasets: [{
+                    data: [178, 218], // Ganti dengan data jumlah penduduk laki-laki dan perempuan
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.5)', // Warna untuk laki-laki 
+                        'rgba(54, 162, 235, 0.5)', // Warna untuk perempuan
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                legend: {
+                    display: false // Sembunyikan legenda bawaan
+                }
             }
-        }
-    });
+        });
 
-    // Tambahkan penjelasan warna
-    var legendItems = document.querySelectorAll('.legend-item');
-    legendItems.forEach(function(item, index) {
-        item.innerHTML += ' (' + myChart.data.datasets[0].data[index] + ')';
-    });
-</script>
+        // Tambahkan penjelasan warna
+        var legendItems = document.querySelectorAll('.legend-item');
+        legendItems.forEach(function(item, index) {
+            item.innerHTML += ' (' + myChart.data.datasets[0].data[index] + ')';
+        });
+    </script>
 
     <script>
         function updateClock() {
@@ -220,26 +339,24 @@
             var hours = now.getHours();
             var minutes = now.getMinutes();
             var seconds = now.getSeconds();
-    
+
             // Format waktu, tambahkan angka 0 di depan jika angka kurang dari 10
             var timeString = pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
-    
+
             // Format hari dan tanggal
             var dateString = day + '<br>' + date + '/' + month + '/' + year;
-    
+
             // Tampilkan waktu, hari, dan tanggal dalam elemen HTML
             clock.innerHTML = timeString + '<br>' + dateString;
         }
-    
+
         function pad(number) {
             return (number < 10 ? '0' : '') + number;
         }
-    
+
         updateClock(); // Panggil fungsi updateClock untuk pertama kali
-    
+
         // Perbarui waktu setiap detik
         setInterval(updateClock, 1000);
     </script>
-    
-    
 @endsection
